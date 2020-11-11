e2c53ce088066144f3ef887d39d37f37dbc0df65
Priyank Agarwal <priyank-ayron@gmail.com>
2020-11-11 23:10:59 +0530

    updating

--- server/index.js
+++ server/index.js
@@ -9,5 +9,11 @@ const io = socketio(server);
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
 
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+  socket.on("disconnect", () => {
+    console.log("user has left.");
+  });
+});
 app.use(router);
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
